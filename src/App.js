3e6afda92cc83fcb1420e5ad7a2925e5c894b936
biggersmatt <biggersmatt73@gmail.com>
2022-02-18 13:50:04 -0800

    Checking if git works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
+
+
+import Button from '@mui/material/Button';
+
+import Homepage from './Homepage/homepage';
+
+require ('./App.css');
+
+export default function App() {
   return (
-    <div>
-      <h1>Jump to Recipe</h1>
+    <div className='app-wrapper'>
+      <Button variant='contained'>Hello World</Button>
+      {/* <Homepage></Homepage> */}
     </div>
   );
 }
-
-export default App;
---
+++ src/Homepage/homepage.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Homepage() {
+  return (
+    <div>
+      <h1>This is the Hompage</h1>
+    </div>
+  )
+}
