de2920bc9cbc01f986aec7047a7083bcb0e57e6a
Matt Biggers <matt.biggers@workday.com>
2022-05-03 16:38:11 -0700

    Cleans up navbar css

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,64 +1,41 @@
 // React
 import { Link } from 'react-router-dom';
 
-// Material UI
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-
 // Images and CSS
 import Chef from '../../images/chef.jpeg';
 require('./navbar.css');
 
 export default function Navbar() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#d7cec7',
-      }
-    }
-  });
-
   return (
     <div className='navbar-wrapper shadow'>
       <div className="logo-wrapper">
         <img src={Chef} alt="chef" className='chef-image' />
       </div>
       <div className="buttons">
-        <ThemeProvider theme={theme}>
-          <Link to='/'>
-            <Button 
-              variant='contained' 
-              size='small' 
-              onClick={() => {
-                console.log("Home Pressed");
-              }}
-            >
-              Home
-            </Button>
+          <Link 
+            to='/'
+            className='nav-links'
+            onClick={() => {
+              console.log("Home Pressed");
+            }}
+          >Home
           </Link>
-          <Link to='/login'>
-            <Button 
-              variant='contained' 
-              size='small' 
-              onClick={() => {
-                console.log("Login Pressed");
-              }}
-            >
-              Login
-            </Button>
+          <Link 
+            to='/login'
+            className='nav-links'
+            onClick={() => {
+              console.log("Login Pressed");
+            }}
+          >Login
           </Link>
-          <Link to='/signup'>
-            <Button
-              variant='contained'
-              size='small'
-              onClick={() => {
-                console.log('Signup Pressed');
-              }}
-            >
-              Signup
-            </Button>
+          <Link 
+            to='/signup'
+            className='nav-links'
+            onClick={() => {
+              console.log('Signup Pressed');
+            }}
+          >Signup
           </Link>
-        </ThemeProvider>
       </div>
     </div>
   )
