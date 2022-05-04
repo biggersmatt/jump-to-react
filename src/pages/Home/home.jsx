4c02d6413cb118f528d9a6eceb1ddf1750cdbd28
Matt Biggers <matt.biggers@workday.com>
2022-05-04 16:14:48 -0700

    Adjusts css on home page

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -7,17 +7,17 @@ require('./home.css');
 export default function Homepage() {
   return (
     <div className="dashboard-wrapper">
-    <div className='dashboard-header'>
-      <h1>Jump to Recipe</h1>
-      <p>Recipes without ads or stories.</p>
+      <div className='dashboard-header'>
+        <h1 className='dashboard-title'>Jump to Recipe</h1>
+        <p>Recipes without ads or stories.</p>
+      </div>
+      <div className="dashboard-cards">
+        <Link to='/myrecipes' className='dashboard-link'>My Recipes</Link>
+        <Link to='/newrecipe' className='dashboard-link'>New Recipe</Link>
+        <Link to='/favorites' className='dashboard-link'>Favorite Recipes</Link>
+        <Link to='/others' className='dashboard-link'>Recipes by Others</Link>
+        <Link to='/everyones' className='dashboard-link'>Everyones Recipes</Link>
+      </div>
     </div>
-    <div className="dashboard-cards">
-      <Link to='/myrecipes' className='dashboard-link'>My Recipes</Link>
-      <Link to='/newrecipe' className='dashboard-link'>New Recipe</Link>
-      <Link to='/favorites' className='dashboard-link'>Favorite Recipes</Link>
-      <Link to='/others' className='dashboard-link'>Recipes by Others</Link>
-      <Link to='/everyones' className='dashboard-link'>Everyones Recipes</Link>
-    </div>
-  </div>
   )
 }
